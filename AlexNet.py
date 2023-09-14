775ba57d8931dcd2044eb14210533f6ce6f93e8b
hotsoycandy <wkdwnsghd617@gmail.com>
2023-09-14 18:28:27 +0900

    Update AlexNet.py

--- AlexNet.py
+++ AlexNet.py
@@ -1,10 +1,7 @@
 from matplotlib import pyplot as plt
 
-import torch
 from torch import nn
-from torchvision.datasets import ImageFolder
-from torchvision import transforms as torchTrans
-from torch.utils.data import DataLoader
+from utils.getDevice import getDevice
 from torchsummary import summary as torchsummary
 
 # 모델을 정의합니다.
@@ -12,7 +9,7 @@ class AlexNet (nn.Module) :
   def __init__ (self) :
     super(AlexNet, self).__init__()
 
-    self.cnn_layers = nn.Sequential(
+    self.layers = nn.Sequential(
       # Layer1. Convolutional Layer
       nn.Conv2d(
         in_channels = 3,
@@ -22,7 +19,7 @@ class AlexNet (nn.Module) :
         padding = 2,
       ),
       nn.ReLU(),
-      torch.nn.LocalResponseNorm(
+      nn.LocalResponseNorm(
         size = 5,
         alpha = 1e-4,
         beta = 0.75,
@@ -42,7 +39,7 @@ class AlexNet (nn.Module) :
         padding = 2,
       ),
       nn.ReLU(),
-      torch.nn.LocalResponseNorm(
+      nn.LocalResponseNorm(
         size = 5,
         alpha = 1e-4,
         beta = 0.75,
@@ -86,9 +83,9 @@ class AlexNet (nn.Module) :
         kernel_size = 3,
         stride = 2,
       ),
-    )
 
-    self.fc_layers = nn.Sequential(
+      nn.Flatten(),
+
       # Layer6. Affine Layer
       nn.Dropout(p = 0.5),
       nn.Linear(9216, 4096),
@@ -101,12 +98,15 @@ class AlexNet (nn.Module) :
       nn.Linear(4096, 1000),
     )
 
-    self.init_weight()
-
   def forward (self, x) :
-    x = self.cnn_layers(x)
-    print(x.flatten())
-    x = x.view(-1, 256 * 6 * 6)
-    print(x)
-    x = self.fc_layers(x)
+    x = self.layers(x)
     return x
+
+if __name__ == '__main__' :
+  device = getDevice()
+  model = AlexNet().to(device)
+  torchsummary(
+    model = model,
+    input_size = (3, 224, 224),
+    device = device
+  )
